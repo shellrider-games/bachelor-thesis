d34db79b99fc9fd73f0db222b78768077f79a058
Georg Becker <georg.becker@shellrider.com>
2023-12-19 11:23:27 +0100

    Setup main file to select correct torch device depending on if cuda is available or not

---
+++ src/sketch_detection/main.py
@@ -0,0 +1,20 @@
+import torch
+
+def select_torch_device():
+    return torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+
+def load_model(path):
+    device = select_torch_device()
+    # model = get_model_instance_segmentation(2)
+    # model.load_state_dict(torch.load(path))
+    # model.to(device)
+    # model.eval()
+    # return model
+
+def main():
+    device = select_torch_device()
+    print(device)
+    print('Hello, World!')
+
+if __name__ == "__main__":
+    main()
