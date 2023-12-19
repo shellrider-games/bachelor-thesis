0420a86da69aa94a07ca67a823d46118c878eb13
Georg Becker <georg.becker@shellrider.com>
2023-12-19 13:28:46 +0100

    Add Thorstens paper to my sources

--- src/sketch_detection/main.py
+++ src/sketch_detection/main.py
@@ -1,15 +1,17 @@
 import torch
 
+from sketch_detection.sketchdetection.train import get_model_instance_segmentation
+
 def select_torch_device():
     return torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 
 def load_model(path):
     device = select_torch_device()
-    # model = get_model_instance_segmentation(2)
-    # model.load_state_dict(torch.load(path))
-    # model.to(device)
-    # model.eval()
-    # return model
+    model = get_model_instance_segmentation(2)
+    model.load_state_dict(torch.load(path))
+    model.to(device)
+    model.eval()
+    return model
 
 def main():
     device = select_torch_device()
